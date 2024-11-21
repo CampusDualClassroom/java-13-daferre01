ab1082d3c28f4c70253134d91e2d2ab805a9e487
David Fernandez Reboredo <davidfr2001@hotmail.com>
2024-11-21 12:36:54 +0100

    Ejercicio hecho

--- src/main/java/com/campusdual/classroom/Exercise13.java
+++ src/main/java/com/campusdual/classroom/Exercise13.java
@@ -5,7 +5,8 @@ public class Exercise13 {
         FuelTank ft = new FuelTank();
         ft.showDetails();
         System.out.println("Actualización capacidad");
-        ft.actualFuel = -8;
+        ft.setActualFuel(-3);
+        ft.setActualFuel(3);
         ft.showDetails();
     }
 }
